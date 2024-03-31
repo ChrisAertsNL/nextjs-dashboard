d0ac4cad8e0c29f8488769b783fd8d4661fa56b3
Chris Aerts <chris@aplusv.solutions>
2024-03-31 14:07:37 +0200

    feat. title added

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -60,7 +60,12 @@ export default function ThreeJsScene() {
     };
   }, []);
 
-  return <div ref={containerRef} />; //style={{ width: '100%', height: '100%' }}
+  return (
+    <>
+      <p>Fascinatio Workstation 01: Wedi</p>
+      <div ref={containerRef} />
+    </>
+  ); //style={{ width: '100%', height: '100%' }}
 }
 
 async function loadIfcAsFragments(scene, fragmentIfcLoader) {
